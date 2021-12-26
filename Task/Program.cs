21b46f58ec6160b4bd72dc1c10456f46dda59d30
Tania Storm <TeteStorm@users.noreply.github.com>
2021-12-25 22:47:45 -0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace TaskExamples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Tasks>();
+        }
+
+    }
+}
---
+++ Task/Tasks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Threading.Tasks;
+
+namespace TaskExamples
+{
+    [MemoryDiagnoser]
+    public class Tasks
+    {
+        [Params(10)]
+        public int Number { get; set; }
+
+        [Benchmark]
+        public Task<int> AddAsyncTask()
+        {
+            return Task.Run(() => this.Number * this.Number);
+        }
+
+        [Benchmark]
+        public Task<int> AddAsyncTaskFromResult()
+        {
+            return Task.FromResult(this.Number * this.Number);
+        }
+
+        [Benchmark]
+        public ValueTask<int> AddAsyncValueTask()
+        {
+            return new ValueTask<int>(this.Number * this.Number);
+        }
+    }
+}
